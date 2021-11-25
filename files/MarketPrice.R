0c82654165fb93635adf8a386d1a2ed9b4937c10
VisheshBansal <vishesh.bansal@gmail.com>
2021-11-26 00:01:48 +0530

    Added Code for Visualization

---
+++ files/MarketPrice.R
@@ -0,0 +1,14 @@
+library(plotly)
+df <- data.frame(read.csv("C:/Users/bansa/Downloads/ElectricCarData_Clean.csv", header=TRUE,sep=","))
+
+data <- df[,c('Brand', 'PriceEuro','Model')]
+fig <- plot_ly(data, labels =~Brand, values = ~PriceEuro, type = 'pie',
+               textposition = 'inside',
+               textinfo = 'label+percent',
+               insidetextfont = list(color = '#FFFFFF'),
+               marker = list(colors = colors,line = list(color = '#FFFFFF', width = 1), pull=1.5),
+               showlegend = FALSE)
+fig <- fig %>% layout( title='Market Price in Euros',
+                       xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                       yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+fig
