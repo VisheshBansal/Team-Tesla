c04e889405d144e770498efaff34def2f87f3e49
Nikita Bisht <71428128+NikitaBisht2605@users.noreply.github.com>
2021-11-26 17:11:45 +0530

    Create compare_Cars_3d.r

---
+++ files/compare_Cars_3d.r
@@ -0,0 +1,11 @@
+library(plotly)
+
+setwd("E:/")
+data<-read.csv("ElectricCarData_Clean.csv")
+fig <- plot_ly(data,x =~Brand,y =~Seats,z =~Segment,color=~Brand,text = ~paste('Brand:', Brand, '<br>Seats:', Seats, '<br>Segment:', Segment))
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'Brand'),
+                                   yaxis = list(title = 'Seats'),
+                                   zaxis = list(title = 'Segment')))
+
+fig
