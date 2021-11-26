d59627f8bdfecc1433b492bde3d9fc368140cbce
ishizzz <71628119+ishizzz@users.noreply.github.com>
2021-11-26 21:13:09 +0530

    Add files via upload

---
+++ files/Heat Map DV.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(plotly)
+library(reshape2)
+d <-read.csv("/Users/NeerajChauhan/Downloads/DV project.csv", header=TRUE)
+
+
+z<-d[1:30,c(14,5,6,7,4)]
+z
+
+cormat <- round(cor(z),4)
+head(cormat)
+melted_cormat <- melt(cormat)
+head(melted_cormat)
+
+fig <- plot_ly(
+  x = c("price","top speed", "range", "efficiency","fast charge"),
+  y = c("price","top speed", "range", "efficiency","fast charge"),
+  z = cormat, type = "heatmap"
+)
+
+fig
